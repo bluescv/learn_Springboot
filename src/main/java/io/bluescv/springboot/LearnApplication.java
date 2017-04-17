a0cb49aa675ca2228ea7a44705dead10f5956438
bluescv <bluescv@gmail.com>
2017-04-17 16:43:09 +0800

    Initial commit

---
+++ src/main/java/io/bluescv/springboot/LearnApplication.java
@@ -0,0 +1,12 @@
+package io.bluescv.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApplication.class, args);
+	}
+}
